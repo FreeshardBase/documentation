261fe64b0772678a8b75c291022776ee3593855c
Max von Tettenborn <max@vtettenborn.net>
2023-10-02 18:38:55 +0200

    add automatic build for master plan

---
+++ docs/master_plan/meta.md
@@ -0,0 +1,20 @@
+---
+title: "Portal Master Plan"
+author: [Max von Tettenborn]
+date: \today
+lang: "en"
+
+titlepage: true
+titlepage-color: "f7f7f7"
+titlepage-text-color: "343a40"
+titlepage-rule-color: "343a40"
+titlepage-logo: "img/logo.png"
+logo-width: "20mm"
+fontcolor: "101010"
+
+toc: true
+toc-own-page: true
+
+page-background: img/bg.png
+page-background-opacity: 1
+...
---
+++ make_latex.py
@@ -0,0 +1,52 @@
+import datetime
+import subprocess
+import sys
+
+import yaml
+
+
+def main():
+	mkdocs_yml = read_mkdocs_yml()
+	chapters = make_chapter_list(mkdocs_yml)
+
+	pandoc_command = 'docker run --rm -v "$(pwd):/data" -u $(id -u):$(id -g) pandoc/extra --template eisvogel '
+	chapters_list = ' '.join(chapters)
+	output_file = make_output_file_name()
+	subprocess.run(f'{pandoc_command} {chapters_list} -o {output_file}', shell=True, cwd='docs/master_plan')
+
+	print(output_file)
+
+
+def read_mkdocs_yml():
+	def default_ctor(loader, tag_suffix, node):
+		return None
+
+	yaml.add_multi_constructor('', default_ctor, Loader=yaml.SafeLoader)
+	with open('mkdocs.yml', 'r') as f:
+		return yaml.load(f, yaml.SafeLoader)
+
+
+def make_chapter_list(mkdocs_yml):
+	nav = mkdocs_yml['nav']
+	master_plan = [item for item in nav if 'Master Plan' in item][0]
+	prefix_len = len('master_plan/')
+	chapters = [c[prefix_len:] for c in master_plan['Master Plan']]
+	chapters = ['meta.md'] + chapters
+	return chapters
+
+
+def make_output_file_name():
+	try:
+		suffix = sys.argv[1]
+	except IndexError:
+		suffix = None
+
+	date = datetime.datetime.now().strftime('%Y-%m-%d')
+	if suffix:
+		return f'master_plan_{date}_{suffix}.pdf'
+	else:
+		return f'master_plan_{date}.pdf'
+
+
+if __name__ == '__main__':
+	main()
